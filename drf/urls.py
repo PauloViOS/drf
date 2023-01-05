5eba83898d94726b8140e23813b6919c4278968f
Paulo Santos <paulo.santos@buser.com.br>
2023-01-05 18:08:36 -0300

    - creates router and registers movie api

--- drf/urls.py
+++ drf/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from movies.views import MovieDataViewset
+
+router = routers.DefaultRouter()
+router.register('movies', MovieDataViewset)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('', include(router.urls)),
+	path('admin/', admin.site.urls),
 ]
